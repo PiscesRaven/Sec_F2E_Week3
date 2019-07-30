b885ff9cbfde683223794f28e95810a2ba9b45cb
Reaven <y93209@gmail.com>
2019-07-30 23:51:16 +0800

    init

---
+++ vue.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@as': '@/assets',
+        '@c': '@/components',
+        '@v': '@/views',
+        '@img': '@/assets/img',
+        '@css': '@/assets/scss',
+        '@api': '@/api',
+      }
+    }
+  },
+  chainWebpack: config => {
+    config.module.rule('pug')
+      .test(/\.pug$/)
+      .use('pug-html-loader')
+      .loader('pug-html-loader')
+      .end()
+  }
+  // css: {
+  //   loaderOptions: {
+  //     sass: {
+  //       data: ` @import "@css/mixin.scss"; @import "@css/main.scss";`
+  //     }
+  //   }
+  // },
+}
